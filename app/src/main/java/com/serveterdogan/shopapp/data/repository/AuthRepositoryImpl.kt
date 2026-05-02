08668b7476941d92c9a6f320bad7fa0dd0ff2f76
Servet Erdoğan <eservet224@gmail.com>
2026-05-02 21:39:01 +0300

    feat: implement AuthRepository and its implementation with error handling"

---
+++ app/src/main/java/com/serveterdogan/shopapp/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.serveterdogan.shopapp.data.repository
+
+import com.serveterdogan.shopapp.data.remote.AuthApiService
+import com.serveterdogan.shopapp.di.AppQualifiers
+import com.serveterdogan.shopapp.domain.model.LoginRequest
+import com.serveterdogan.shopapp.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val apiService: AuthApiService
+) : AuthRepository {
+    override suspend fun login(loginRequest: LoginRequest): Result<String> {
+     return  try {
+          val response = apiService.login(loginRequest)
+         Result.success(response.token)
+       }catch (e: Exception){
+         Result.failure(e)
+       }
+    }
+
+    override suspend fun register(loginRequest: LoginRequest): Result<String> {
+        return  try {
+            val response = apiService.register(loginRequest)
+            Result.success(response.token)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/serveterdogan/shopapp/domain/repository/AuthRepository.kt
@@ -0,0 +1,11 @@
+package com.serveterdogan.shopapp.domain.repository
+
+import com.serveterdogan.shopapp.data.remote.dto.LoginResponse
+import com.serveterdogan.shopapp.domain.model.LoginRequest
+
+interface AuthRepository {
+
+    suspend fun login(loginRequest: LoginRequest) : Result<String>
+
+    suspend fun register(loginRequest: LoginRequest) : Result<String>
+}
