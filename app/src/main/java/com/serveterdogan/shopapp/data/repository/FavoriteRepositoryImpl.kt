919717369cf2a2c3b21d767cdaa53e554bfb5551
Servet Erdoğan <eservet224@gmail.com>
2026-05-05 20:30:35 +0300

    feat: implement ProductDao for local product persistence and favorite management

---
+++ app/src/main/java/com/serveterdogan/shopapp/data/repository/FavoriteRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.serveterdogan.shopapp.data.repository
+
+import com.serveterdogan.shopapp.data.local.Entity.ProductDao
+import com.serveterdogan.shopapp.data.local.Entity.ProductEntity
+import com.serveterdogan.shopapp.data.mapper.toProduct
+import com.serveterdogan.shopapp.data.mapper.toProductEntity
+import com.serveterdogan.shopapp.domain.model.Product
+import com.serveterdogan.shopapp.domain.repository.FavoriteRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class FavoriteRepositoryImpl @Inject constructor(
+    private val dao: ProductDao
+) : FavoriteRepository{
+    override fun getFavoriteProducts(): Flow<List<Product>> {
+       return dao.getFavoriteProducts().map { list->
+           list.map { it.toProduct()}
+       }
+    }
+
+    override suspend fun addFavoriteProduct(product: Product): Result<Unit> {
+        return try {
+            dao.insertProduct(product.toProductEntity())
+            Result.success(Unit) // herhnagi bişi döndürmüyeceğiz Unit
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun deleteFavoriteProduct(id: Int): Result<Unit> {
+        return try {
+           dao.deleteProduct(id)
+            Result.success(Unit)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+
+    override fun isProductFavorite(productId: Int): Flow<Boolean> {
+        return dao.isFavorite(productId)
+    }
+}
---
+++ app/src/main/java/com/serveterdogan/shopapp/domain/repository/FavoriteRepository.kt
@@ -0,0 +1,17 @@
+package com.serveterdogan.shopapp.domain.repository
+
+import com.serveterdogan.shopapp.data.local.Entity.ProductEntity
+import com.serveterdogan.shopapp.domain.model.Product
+import kotlinx.coroutines.flow.Flow
+
+
+interface FavoriteRepository {
+
+    fun getFavoriteProducts(): Flow<List<Product>>
+    suspend fun addFavoriteProduct(product: Product) : Result<Unit>
+    suspend fun deleteFavoriteProduct(id: Int) : Result<Unit>
+
+    // ürün favori mi değil mi kontrol etmek için
+    fun isProductFavorite(productId: Int): Flow<Boolean>
+
+}
