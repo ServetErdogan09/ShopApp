cd89d8e5c1d49ca23c6e8fc15a9b3b65f3c50e40
Servet Erdoğan <eservet224@gmail.com>
2026-05-05 20:25:34 +0300

    feat: implement ProductDao for local product persistence and favorite management

---
+++ app/src/main/java/com/serveterdogan/shopapp/data/local/Entity/ProductDao.kt
@@ -0,0 +1,27 @@
+package com.serveterdogan.shopapp.data.local.Entity
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ProductDao {
+
+    @Query("SELECT * FROM products")
+    fun getFavoriteProducts() : Flow<List<ProductEntity>> // Favoriler değiştiği zaman bu listeyi günceller anlık olarak bize veriyor
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // eğer aynı id ye sahip bir ürün varsa onu güncele
+    suspend fun insertProduct(productEntity: ProductEntity)
+
+
+    @Query("DELETE FROM products WHERE id = :id")
+    suspend fun deleteProduct(id: Int)
+
+    @Query("SELECT EXISTS(SELECT 1 FROM products WHERE id = :id)")
+    fun isFavorite(id: Int): Flow<Boolean>
+}
+
