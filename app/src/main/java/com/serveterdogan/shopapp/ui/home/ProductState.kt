1f52eac1196dbe6bfd731fe459725c489286201c
Servet Erdoğan <eservet224@gmail.com>
2026-05-05 10:03:16 +0300

    feat: implement ProductState for home UI state management

---
+++ app/src/main/java/com/serveterdogan/shopapp/ui/home/ProductState.kt
@@ -0,0 +1,12 @@
+package com.serveterdogan.shopapp.ui.home
+
+import com.serveterdogan.shopapp.domain.model.Product
+
+data class ProductState(
+    val products : List<Product> = emptyList(),
+    val isLoading : Boolean = false,
+    val selectedProduct : Product? = null, // detay sayfası için
+    val isError: String? = null,
+    val isFromCache : Boolean = false,
+    val lastUpdate : Long? = null
+)
