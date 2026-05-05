9f225b0a521da9a2fbfce31ca6cb890d5d16d866
Servet Erdoğan <eservet224@gmail.com>
2026-05-05 10:48:33 +0300

    feat: add selectedCategory field to ProductState

--- app/src/main/java/com/serveterdogan/shopapp/ui/home/ProductState.kt
+++ app/src/main/java/com/serveterdogan/shopapp/ui/home/ProductState.kt
@@ -7,6 +7,7 @@ data class ProductState(
     val isLoading : Boolean = false,
     val selectedProduct : Product? = null, // detay sayfası için
     val isError: String? = null,
+    val selectedCategory: String = "Tümü",
     val isFromCache : Boolean = false,
     val lastUpdate : Long? = null
 )
