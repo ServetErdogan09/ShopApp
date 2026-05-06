60682fb66d1d4eae93cbf83a98fb6696d8afcc27
Servet Erdoğan <eservet224@gmail.com>
2026-05-06 10:42:52 +0300

    feat: create ProductEntity for local Room database support

---
+++ app/src/main/java/com/serveterdogan/shopapp/data/local/Entity/ProductEntity.kt
@@ -0,0 +1,19 @@
+package com.serveterdogan.shopapp.data.local.Entity
+
+import android.media.Rating
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class ProductEntity (
+    @PrimaryKey
+    val id : Int,
+    val title : String,
+    val price : Double,
+    val thumbnail : String,
+    val rating: Double,
+    val category: String,
+    val brand: String,
+    val lastUpdated: Long
+
+)
