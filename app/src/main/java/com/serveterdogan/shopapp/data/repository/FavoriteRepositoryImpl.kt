73b5a73f956c1100735a4b98b6411817cc2813d6
Servet Erdoğan <eservet224@gmail.com>
2026-05-06 10:40:10 +0300

    feat: implement refreshFavoriteProducts in FavoriteRepositoryImpl to sync favorites with remote data

--- app/src/main/java/com/serveterdogan/shopapp/data/repository/FavoriteRepositoryImpl.kt
+++ app/src/main/java/com/serveterdogan/shopapp/data/repository/FavoriteRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.serveterdogan.shopapp.data.local.Entity.ProductDao
 import com.serveterdogan.shopapp.data.local.Entity.ProductEntity
 import com.serveterdogan.shopapp.data.mapper.toProduct
 import com.serveterdogan.shopapp.data.mapper.toProductEntity
+import com.serveterdogan.shopapp.data.remote.service.ProductApiService
 import com.serveterdogan.shopapp.domain.model.Product
 import com.serveterdogan.shopapp.domain.repository.FavoriteRepository
 import kotlinx.coroutines.flow.Flow
@@ -11,7 +12,8 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class FavoriteRepositoryImpl @Inject constructor(
-    private val dao: ProductDao
+    private val dao: ProductDao,
+    private val apiService: ProductApiService
 ) : FavoriteRepository{
     override fun getFavoriteProducts(): Flow<List<Product>> {
        return dao.getFavoriteProducts().map { list->
@@ -40,4 +42,22 @@ class FavoriteRepositoryImpl @Inject constructor(
     override fun isProductFavorite(productId: Int): Flow<Boolean> {
         return dao.isFavorite(productId)
     }
+
+    override suspend fun refreshFavoriteProducts(): Result<Unit> {
+        return try {
+
+            val currentFavorites = dao.getFavoriteProductsOnce()
+
+            currentFavorites.forEach { entity ->
+                val response = apiService.getProductById(entity.id)
+                // API başarılıysa veriyi güncelle
+                dao.insertProduct(response.toProduct().toProductEntity())
+            }
+            
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
 }
