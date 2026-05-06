31ac9fb0c986813b6db713db7646b2ea658b2ac8
Servet Erdoğan <eservet224@gmail.com>
2026-05-06 10:36:24 +0300

    feat: update CartDao with quantity increment, decrement, and single item retrieval logic

--- app/src/main/java/com/serveterdogan/shopapp/data/local/Entity/CartDao.kt
+++ app/src/main/java/com/serveterdogan/shopapp/data/local/Entity/CartDao.kt
@@ -8,17 +8,25 @@ interface CartDao {
     @Query("SELECT * FROM cart_items")
     fun getAllCartItems(): Flow<List<CartEntity>>
 
+    @Query("SELECT * FROM cart_items WHERE id = :productId LIMIT 1")
+    suspend fun getOneCartItem(productId: Int): CartEntity?
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCartItem(cartEntity: CartEntity)
 
     @Delete
     suspend fun deleteCartItem(cartEntity: CartEntity)
 
+    @Query("UPDATE cart_items SET quantity = quantity - 1  WHERE id = :productId AND quantity > 1")
+    suspend fun decrementIfExists(productId: Int): Int
+
+    @Query("DELETE FROM cart_items WHERE id = :productId AND quantity = 1")
+    suspend fun deleteIfSingle(productId: Int) : Int
+
+    @Query("UPDATE cart_items SET  quantity = quantity +1 WHERE id = :productId")
+    suspend fun incrementIfExists(productId: Int) : Int
     @Query("DELETE FROM cart_items WHERE id = :productId")
     suspend fun deleteCartItemById(productId: Int)
-
-    @Query("UPDATE cart_items SET quantity = :newQuantity WHERE id = :productId")
-    suspend fun updateQuantity(productId: Int, newQuantity: Int)
 
     @Query("DELETE FROM cart_items")
     suspend fun clearCart()
