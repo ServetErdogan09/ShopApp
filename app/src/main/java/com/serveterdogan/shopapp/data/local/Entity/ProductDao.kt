0626d0efc0ccdfaf296673144cc7d8d375ae7e07
Servet Erdoğan <eservet224@gmail.com>
2026-05-06 10:44:00 +0300

    feat: add method to retrieve favorite products once in ProductDao

--- app/src/main/java/com/serveterdogan/shopapp/data/local/Entity/ProductDao.kt
+++ app/src/main/java/com/serveterdogan/shopapp/data/local/Entity/ProductDao.kt
@@ -13,6 +13,9 @@ interface ProductDao {
     @Query("SELECT * FROM products")
     fun getFavoriteProducts() : Flow<List<ProductEntity>> // Favoriler değiştiği zaman bu listeyi günceller anlık olarak bize veriyor
 
+    @Query("SELECT * FROM products")
+    suspend fun getFavoriteProductsOnce(): List<ProductEntity>
+
 
     @Insert(onConflict = OnConflictStrategy.REPLACE) // eğer aynı id ye sahip bir ürün varsa onu güncele
     suspend fun insertProduct(productEntity: ProductEntity)
