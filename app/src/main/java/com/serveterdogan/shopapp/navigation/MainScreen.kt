3d89893b3179c83d6f28051a16fd509566cc6fe0
Servet Erdoğan <eservet224@gmail.com>
2026-05-06 10:46:12 +0300

    feat: implement navigation and state management for Favorite, Cart, and Profile screens

--- app/src/main/java/com/serveterdogan/shopapp/navigation/MainScreen.kt
+++ app/src/main/java/com/serveterdogan/shopapp/navigation/MainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -11,20 +12,24 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.serveterdogan.shopapp.data.local.TokenManager
 import com.serveterdogan.shopapp.ui.cart.CartScreen
 import com.serveterdogan.shopapp.ui.favorite.FavoriteScreen
+import com.serveterdogan.shopapp.ui.favorite.FavoriteViewModel
 import com.serveterdogan.shopapp.ui.home.ProductScreen
 import com.serveterdogan.shopapp.ui.home.ProductViewModel
 import com.serveterdogan.shopapp.ui.profile.ProfileScreen
+import com.serveterdogan.shopapp.ui.register.RegisterViewmodel
 
 @Composable
-fun MainScreen(rootNavController: NavHostController) {
+fun MainScreen(rootNavController: NavHostController , tokenManager: TokenManager) {
     val navController = rememberNavController()
 
     Scaffold(
         bottomBar = {
             BottomBar(navController = navController)
-        }
+        },
+        contentWindowInsets = androidx.compose.foundation.layout.WindowInsets(0)
     ) { paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
             NavHost(
@@ -39,19 +44,46 @@ fun MainScreen(rootNavController: NavHostController) {
                         state = product.value,
                         onSearch = productViewModel::getSearchProduct,
                         onCategoryClick = productViewModel::getProductCategory,
+                        onFavoriteClick = productViewModel::toggleFavorite,
                         onProductClick = { productId ->
                             rootNavController.navigate(Screen.ProductDetails.createRoute(productId))
                         }
                     )
                 }
                 composable(Screen.Favorite.route) {
-                    FavoriteScreen()
+                    val favoriteViewModel: FavoriteViewModel = hiltViewModel()
+                    val favoriteState = favoriteViewModel.state.collectAsStateWithLifecycle()
+
+                    FavoriteScreen(
+                        state = favoriteState.value,
+                        onProductClick = { productId ->
+                            rootNavController.navigate(Screen.ProductDetails.createRoute(productId))
+                        },
+                        onRemoveFavorite = { productId ->
+                            favoriteViewModel.deleteProductFavorite(productId)
+                        }
+                    )
                 }
                 composable(Screen.Cart.route) {
-                    CartScreen()
+                    val cartViewModel: com.serveterdogan.shopapp.ui.cart.CartViewModel = hiltViewModel()
+                    val cartState = cartViewModel.state.collectAsStateWithLifecycle()
+
+                    CartScreen(
+                        state = cartState.value,
+                        onIncrement = cartViewModel::incrementQuantity,
+                        onDecrement = cartViewModel::decrementQuantity,
+                        onRemove = cartViewModel::removeFromCart,
+                        onCheckoutClick = { /* Ödeme sayfası henüz yok */ }
+                    )
                 }
                 composable(Screen.Profile.route) {
-                    ProfileScreen()
+                    val viewmodel : RegisterViewmodel = hiltViewModel()
+                    ProfileScreen(
+                        onLogout = {
+                            //rootNavController.navigate(Screen.Login.route)
+                            viewmodel.clearToken()
+                        }
+                    )
                 }
             }
         }
