98a2a47b3d210bb640929fef7c5ea52aed75bf1e
Servet Erdoğan <eservet224@gmail.com>
2026-05-06 10:37:02 +0300

    feat: implement CartRepositoryImpl for cart data management and local storage interaction

---
+++ app/src/main/java/com/serveterdogan/shopapp/data/repository/CartRepositoryImpl.kt
@@ -0,0 +1,61 @@
+package com.serveterdogan.shopapp.data.repository
+
+import android.util.Log
+import com.serveterdogan.shopapp.data.local.Entity.CartDao
+import com.serveterdogan.shopapp.data.local.Entity.CartEntity
+import com.serveterdogan.shopapp.domain.model.Product
+import com.serveterdogan.shopapp.domain.repository.CartRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class CartRepositoryImpl @Inject constructor(
+    private val cartDao: CartDao
+) : CartRepository {
+
+    override fun getCartItems(): Flow<List<CartEntity>> = cartDao.getAllCartItems()
+
+    override suspend fun addToCart(product: Product) {
+
+        //val existingItem = cartDao.getOneCartItem(product.id)
+        val update = cartDao.incrementIfExists(productId = product.id)
+
+        Log.d("CartRepositoryImpl", "addToCart called with productId: ${product.id}")
+        if (update == 0) {
+            cartDao.insertCartItem(
+                CartEntity(
+                    id = product.id,
+                    title = product.title,
+                    price = product.price,
+                    thumbnail = product.thumbnail,
+                    quantity = 1,
+                    category = product.category ?: "",
+                    brand = product.brand
+                )
+            )
+        }
+    }
+
+    override suspend fun removeFromCart(productId: Int) {
+        Log.d("CartRepositoryImpl", "removeFromCart called with productId: $productId")
+        cartDao.deleteCartItemById(productId)
+    }
+
+    override suspend fun incrementQuantity(productId: Int) {
+        Log.d("CartRepositoryImpl", "incrementQuantity called with productId: $productId")
+       cartDao.incrementIfExists(productId = productId)
+    }
+
+    override suspend fun decrementQuantity(productId: Int) {
+        Log.d("CartRepositoryImpl", "decrementQuantity called with productId: $productId")
+       val update = cartDao.decrementIfExists(productId = productId)
+
+        if(update == 0){
+            cartDao.deleteIfSingle(productId = productId)
+        }
+    }
+
+    override suspend fun clearCart() {
+        cartDao.clearCart()
+    }
+}
