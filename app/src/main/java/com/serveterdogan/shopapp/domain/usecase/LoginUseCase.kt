17ae7f8a978c8d4bf1235e05da5456dbdb793470
Servet Erdoğan <eservet224@gmail.com>
2026-05-21 14:55:49 +0300

    feat: implement LoginUseCase with input validation and authentication repository integration

---
+++ app/src/main/java/com/serveterdogan/shopapp/domain/usecase/LoginUseCase.kt
@@ -0,0 +1,26 @@
+package com.serveterdogan.shopapp.domain.usecase
+
+import com.serveterdogan.shopapp.domain.model.LoginRequest
+import com.serveterdogan.shopapp.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val repository: AuthRepository
+) {
+
+    suspend operator fun invoke(
+        request: LoginRequest
+    ) : Result<String>{
+
+      if(request.password.isEmpty() || request.email.isEmpty()){
+          return Result.failure(Exception("Lütfen tüm alanları doldurun"))
+      }
+
+      if(request.password.length <= 6){
+          return Result.failure(Exception("Şifre en az 6 karakter olmalıdır"))
+      }
+
+      return  repository.login(request)
+
+    }
+}
